b8f761a7c7db22bb2440e3c60cc380e2db673a55
Kamil Kopyra <kamilkopyra@student.edu.pl>
2026-04-02 02:50:50 +0200

    Test

--- Test_Kamil.py
+++ Test_Kamil.py
@@ -1,53 +1,51 @@
+
+
+
+
  # Initialize connection\n
 from Application.Services.MatekService import MatekService
+from Application.Services.MissionService import MissionService
 
 
-
-drone = MatekService(device="tcp:192.168.0.186:5763")
+resolution = [1920, 1080]
+drone = MatekService(device="tcp:172.20.10.2:5763")
+drone_mission = MissionService(drone,resolution) 
 print("Inicjalizacja programu")
 
 
-# DZIAŁA
-#drone.is_armed()
-#drone.arm()
-#drone.get_current_mode()
-#drone.set_mode()
 
+print(drone.get_current_mode())
 
 waypoints = [
     {"command": "WAYPOINT", "lat": -35.363261, "lon": -100.165230, "alt": 20, "acr": 0},
     {"command": "WAYPOINT", "lat": -35.363500, "lon": -110.165500, "alt": 25, "acr": 0},
-    {"command": "SET_SERVO", "channel": 5, "pwm": 1900},
+    {"command": "WAYPOINT", "lat": -35.363500, "lon": -110.165500, "alt": 25, "acr": 0},
 ]
 append = [
     {"command": "WAYPOINT", "lat": -35.363800, "lon": 100.165800, "alt": 20, "acr": 0},
     {"command": "WAYPOINT", "lat": -35.364000, "lon": 110.166000, "alt": 25, "acr": 0},
-    {"command": "SET_SERVO", "channel": 5, "pwm": 1900},
+
 ]
 prepend = [
     {"command": "WAYPOINT", "lat": -35.364200, "lon": 120.166200, "alt": 20, "acr": 0},
     {"command": "WAYPOINT", "lat": -35.364400, "lon": 130.166400, "alt": 25, "acr": 0},
 ]
 
-drone.get_mission()
-drone.set_waypoints(waypoints)
-print("Mission uploaded successfully")
+droppoint = {"command": "WAYPOINT", "lat": -33.0000, "lon": 149.165230, "alt": 20, "acr": 0}
 
-drone.get_mission()
-print("get_mission() wykonane")
+#prosta implementacja dodania zrzutu do obecnej misji
 
-drone.append_waypoints(append)
-print("append_waypoint() wykonane")
-drone.get_mission()
+mission = drone.get_mission()
+print("Mission read successfully")
+cur_waypoint, waypoint_number = drone.get_mission_status()
 
-drone.prepend_waypoints(prepend)
-print("prepend_waypoint() wykonane")
-drone.get_mission()
 
-drone.set_current_waypoint(5)
-print("set_current_waypoint() wykonane")
 
-drone.add_drop_waypoint({"command": "WAYPOINT", "lat": -25.364600, "lon": 140.166600, "alt": 20, "acr": 0})
-print("add_drop_waypoint() wykonane")
+container = drone_mission.calc_drop_waypoints(droppoint, yaw=10, container=[], isRed=True)
+new_mission = drone.add_drop_sequence(container)
+drone.append_waypoints(new_mission)
 
-drone.get_mission()
+
+
+
+
