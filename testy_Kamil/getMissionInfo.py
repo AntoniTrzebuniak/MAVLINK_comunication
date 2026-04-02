351445c967bebb6fe262858e11ba992db4b39192
Kamil Kopyra <kamilkopyra@student.edu.pl>
2026-04-02 03:26:33 +0200

    dodano skrypty testowe

---
+++ testy_Kamil/getMissionInfo.py
@@ -0,0 +1,19 @@
+# Initialize connection\n
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+# Initialize connection
+from Application.Services.MatekService import MatekService
+from Application.Services.MissionService import MissionService
+
+
+
+resolution = [1920, 1080]
+drone = MatekService(device="tcp:172.20.10.2:5763")
+drone_mission = MissionService(drone,resolution) 
+print("Inicjalizacja programu")
+
+print(drone.get_current_mode())
+print(drone.get_mission_status())
+print(drone.get_attitude())
